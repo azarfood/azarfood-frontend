1515c02a0c2ea6d1ee1d2ef961b84944f3a9da65
Sina Salahshour <sina.salahshour.32@gmail.com>
2023-09-24 13:05:40 +0330

    refactor: move tailwind components to config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import type { PluginCreator } from 'tailwindcss/types/config';
+
+const gridLayout: PluginCreator = ({ addComponents }) => {
+	addComponents({
+		'.wrapper': {
+			display: 'grid',
+			'grid-template-columns': '1fr min(120ch, 100%) 1fr',
+		},
+		'.wrapper > *': {
+			'grid-column': '2',
+		},
+		'.full-bleed': {
+			width: '100%',
+			'grid-column': '1 / 4',
+		},
+	});
+};
 
 const config: Config = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	plugins: [gridLayout],
+};
+export default config;
