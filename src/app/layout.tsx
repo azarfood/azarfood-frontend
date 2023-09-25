52675b37c0c1c050492814533317e61bf69a9e26
Sina Salahshour <sina.salahshour.32@gmail.com>
2023-09-25 22:50:12 +0330

    fix: hardcode language to fa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import '@/configs/globals.css';
 import { cn } from '@/utils/cn';
 import { Montserrat } from 'next/font/google';
 import { NextIntlClientProvider } from 'next-intl';
-import { notFound } from 'next/navigation';
 import { Providers } from '@/store/providers';
 import React from 'react';
 import type { Metadata } from 'next';
@@ -17,23 +16,12 @@ export const metadata: Metadata = {
 	description: 'Where great food meets great minds.',
 };
 
-const RootLayout = async ({
-	children,
-	params: { locale },
-}: {
-	children: React.ReactNode;
-	params: { locale: string };
-}) => {
-	let messages = (await import(`../../messages/fa.json`)).default;
-	try {
-		messages = (await import(`../../messages/${locale}.json`)).default;
-	} catch (error) {
-		notFound();
-	}
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+	let messages = (await import(`@/messages/fa.json`)).default;
 	return (
-		<html lang="en" dir="rtl">
+		<html lang="fa" dir="rtl">
 			<body className={cn(montserrat.className, 'wrapper')}>
-				<NextIntlClientProvider locale={locale} messages={messages}>
+				<NextIntlClientProvider locale={'fa'} messages={messages}>
 					<Providers>{children}</Providers>
 				</NextIntlClientProvider>
 			</body>
