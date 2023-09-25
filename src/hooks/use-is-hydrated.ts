144b0b55113a69833cab2463c97187e21af9ff15
Sina Salahshour <sina.salahshour.32@gmail.com>
2023-09-25 22:44:41 +0330

    chore: document format

--- src/hooks/use-is-hydrated.ts
+++ src/hooks/use-is-hydrated.ts
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export const useIsHydrated = () => {
-    const [ isHydrated, setIsHydrated ] = useState(false);
-    useEffect(() => {
-        if (typeof window !== "undefined") {
-            setIsHydrated(true);
-        }
-    }, []);
-    return isHydrated;
+	const [isHydrated, setIsHydrated] = useState(false);
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			setIsHydrated(true);
+		}
+	}, []);
+	return isHydrated;
 };
