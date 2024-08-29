d4c56d2ae70c626ff174c7d7fee0bca542f617f6
Sina Salahshour <sina.salahshour.32@gmail.com>
2024-08-29 15:47:59 +0330

    chore: new ci test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className='flex h-full w-full items-center justify-center gap-4'>
       {t('hello')}
-      <span className='block h-fit w-fit -rotate-90'>:-)</span>
+      <span className='block h-fit w-fit -rotate-90 text-green-600'>:-)</span>
     </main>
   );
 }
