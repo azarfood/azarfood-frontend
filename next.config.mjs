a8f5e9194abfc136d2cf840be2181f94b62a4fff
Sina Salahshour <sina.salahshour.32@gmail.com>
2024-08-29 12:59:52 +0330

    feat: svgr config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    );
+    config.module.rules.push({
+      ...fileLoaderRule,
+      test: /\.svg$/i,
+      resourceQuery: /url/, // *.svg?url
+    });
+    config.module.rules.push({
+      test: /\.svg$/i,
+      resourceQuery: { not: /url/ },
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import './globals.css';
+import '@/configs/globals.css';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import { cn } from '@/utils/cn';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='en' className="h-full">
+      <body className={cn(inter.className, "h-full")}>{children}</body>
     </html>
   );
 }
