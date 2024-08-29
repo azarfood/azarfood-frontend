e1b120f20ea1002608c9b05ff63d2e94459d3be3
Sina Salahshour <sina.salahshour.32@gmail.com>
2024-08-29 17:02:34 +0330

    feat: add theme colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   const t = useI18n();
   return (
     <main className='flex h-full w-full items-center justify-center gap-4'>
-      {t('hello')}
-      <span className='block h-fit w-fit -rotate-90 text-green-600'>:-)</span>
+      <div className='text-secondary-100'>{t('hello')}</div>
+      <span className='block h-fit w-fit -rotate-90 text-primary-100'>:-)</span>
     </main>
   );
 }
