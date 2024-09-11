c91bc39a2d3793f2b9ec863a9b1129b552cb331b
Asra <asbcd@gamil.com>
2024-09-11 23:19:55 +0330

    feat: create user panel page , add user-info frame

---
+++ src/app/(private)/user/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import PersonalInfo from '@/components/personal-info-frame/personal-info-frame.component'
+
+export default function UserPanel(){
+
+    return(
+        <div className="w-[310px] mt-[68px] mx-auto">
+            <PersonalInfo />
+        </div>
+    )
+}
