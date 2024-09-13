afe3148d5181a6d2db1d9986a3d95d512e346a08
Asra <asbcd@gamil.com>
2024-09-13 15:16:44 +0330

    fix: change button component format

--- src/components/user-panel/button/button.component.tsx
+++ src/components/user-panel/button/button.component.tsx
@@ -1,30 +1,15 @@
-import Calculator from '@/assets/icons/user-panel/calculator.svg'
-
 export interface ButtonProps{
-    iconCode: number,
+    icon: React.ReactNode,
     context: string
 }
 
-export default function Button({iconCode , context}: ButtonProps){
+export default function Button({icon , context}: ButtonProps){
     
     return(
         <button className='flex flex-row w-full bg-primary-100 rounded-lg my-4 h-10 drop-shadow-simple-01
-                           items-center text-foreground-100 text-[14px]'>
-
-            {iconCode === 1 && <>
-                              <Calculator className='w-5 h-5 mr-3'/>
-                              <p className='mr-2'>{context}</p>
-                             </>}
-
-            {iconCode === 2 && <>
-                              <Calculator className='w-5 h-5 mr-3'/>
-                              <p className='mr-2'>{context}</p>
-                             </>}
-
-            {iconCode === 3 && <>
-                              <Calculator className='w-5 h-5 mr-3'/>
-                              <p className='mr-2'>{context}</p>
-                             </>}
+                           items-center text-foreground-100 text-[14px] gap-2'>
+            {icon}
+            <p>{context}</p>                     
         </button>
     )
 }
