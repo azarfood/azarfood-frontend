924b9424acb870d911085f68608928f4bd207673
Asra <asbcd@gamil.com>
2024-09-13 15:19:55 +0330

    fix: use Button components in their new format

--- src/app/(private)/user/page.tsx
+++ src/app/(private)/user/page.tsx
@@ -1,21 +1,28 @@
 'use client'
-import { button } from 'framer-motion/client'
 
+import Calculator from '@/assets/icons/user-panel/calculator.svg'
+import Calendar  from '@/assets/icons/user-panel/calendar.svg'
+import CalendarDays  from '@/assets/icons/user-panel/calendar-days.svg'
 import Button from '@/components/user-panel/button/button.component'
-import PersonalInfo from '@/components/user-panel/personal-info-frame.component'
+import PersonalInfo from '@/components/user-panel/personal-info-frame/personal-info-frame.component'
 import { useScopedI18n } from '@/locales/client'
 
 export default function UserPanel(){ 
 
     const t = useScopedI18n("personal_info")
-    
+
     return(
-        <div className="w-[310px] mt-[68px] mx-auto">
+        <div className="w-[310px] mt-12 mx-auto">
             <PersonalInfo />
 
-            <Button iconCode={1} context={t('button.transaction_history')}/>
-            <Button iconCode={2} context={t('button.order_history')}/>
-            <Button iconCode={3} context={t('button.reservation_history')}/>
+            <Button icon={<Calculator className='size-5 mr-3'/>}
+                    context={t('button.transaction_history')}/>
+
+            <Button icon={<Calendar className='size-5 mr-3'/>}
+                    context={t('button.order_history')}/>
+
+            <Button icon={<CalendarDays className='size-5 mr-3'/>}
+                    context={t('button.reservation_history')}/>
 
             <button className='flex flex-row w-full bg-success-100 rounded-lg pr-4 my-4 h-10 drop-shadow-simple-01
                                items-center text-foreground-100 text-[14px]'>
