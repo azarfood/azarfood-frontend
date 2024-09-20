daa9777b16fd012471116608d450eaf51d6c2457
Sina Salahshour <sina.salahshour.32@gmail.com>
2024-09-20 20:58:54 +0330

    feat: create main layout

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,18 +1,12 @@
 import type { PropsWithChildren } from 'react';
 
-import SecondaryLogo from '@/assets/icons/secondary-logo.svg';
+import { MainLayout } from '@/components/main-layout/main-layout.component';
 import { AuthGuard } from '@/stores/providers/auth-provider/auth-guard';
 
 export default function PrivateLayout({ children }: PropsWithChildren) {
   return (
     <AuthGuard>
-      <div className='relative mx-auto flex h-full min-h-svh w-full max-w-[430px] flex-col bg-foreground-100'>
-        <header className='sticky top-0 flex h-16 w-full flex-shrink-0 justify-end bg-secondary-100 px-6 py-5'>
-          <SecondaryLogo className='h-full w-fit' />
-        </header>
-        <main className='flex-1 overflow-auto px-5'>{children}</main>
-        <footer className='h-12 w-full flex-shrink-0 bg-secondary-100'></footer>
-      </div>
+      <MainLayout>{children}</MainLayout>
     </AuthGuard>
   );
 }
