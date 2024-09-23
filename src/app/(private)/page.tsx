4bcbe0ea15eef86d5c147f6cdb6c0fff76ed56d5
Asra <imcengineer03@gmail.com>
2024-09-23 16:19:54 +0330

    feat: add food category , slider

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,12 +1,24 @@
 'use client';
-import { useQuery } from '@tanstack/react-query';
 
+import Slider from '@/components/(home)/ads-slider/ads-slider.component';
+import FoodCategory from '@/components/(home)/food-category/food-category.component';
 import Searchbar from '@/components/searchbar/searchbar.component';
+import { foodCategories } from '@/configs/constants/food-categories.constants';
 
 export default function Home() {
+  const categories = foodCategories.map((item) => (
+    <FoodCategory image={item.image} category={item.category} key='' />
+  ));
+
   return (
     <>
       <Searchbar />
+
+      <div className='mb-4 mt-7 flex flex-row gap-5 overflow-y-hidden overflow-x-scroll p-1 pb-3'>
+        {categories}
+      </div>
+
+      <Slider />
     </>
   );
 }
