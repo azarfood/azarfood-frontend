fa97ff1f561066fdb57099315231d2cfd6b88856
Asra <imcengineer03@gmail.com>
2024-10-10 15:45:08 +0330

    feat: add collection component

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,24 +1,25 @@
 'use client';
 
 import Slider from '@/components/(home)/ads-slider/ads-slider.component';
+import Collecttion from '@/components/(home)/collection/collection.component';
 import FoodCategory from '@/components/(home)/food-category/food-category.component';
 import Searchbar from '@/components/searchbar/searchbar.component';
 import { foodCategories } from '@/configs/constants/food-categories.constants';
 
 export default function Home() {
-  const categories = foodCategories.map((item) => (
-    <FoodCategory image={item.image} category={item.category} key='' />
-  ));
-
   return (
     <>
       <Searchbar />
 
       <div className='mb-4 mt-7 flex flex-row gap-5 overflow-y-hidden overflow-x-scroll p-1 pb-3'>
-        {categories}
+        {foodCategories.map((item) => (
+          <FoodCategory image={item.image} category={item.category} key='' />
+        ))}
       </div>
 
       <Slider />
+
+      <Collecttion />
     </>
   );
 }
