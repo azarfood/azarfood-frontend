c049d645bd7b73b9e5d64b836b2820e4d8a31902
Sina Salahshour <sina.salahshour.32@gmail.com>
2024-10-11 02:53:20 +0330

    fix: remove duplicate home route and add missing types

--- src/app/(private)/(home)/products/food/[id]/page.tsx
+++ src/app/(private)/(home)/products/food/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import Image from 'next/image';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 import AddIcon from '@/assets/icons/add.svg';
 import ArrowLeftIcon from '@/assets/icons/arrow-left.svg';
