7be62ef5c462fcf7a27aac60e79223ab6618f02f
Asra <imcengineer03@gmail.com>
2024-10-11 20:28:48 +0330

    feat: add reastaurant grid

---
+++ src/components/(products)/restaurant-grid/restaurant-grid.component.tsx
@@ -0,0 +1,8 @@
+import Restaurant from "@/components/restaurant/restaurant.component"
+
+export default function RestaurantGrid() {
+  return(
+    <div className='mt-4 mx-5 grid grid-cols-2 gap-x-9 gap-y-6 items-center text-center'>
+    </div>
+  )
+}
