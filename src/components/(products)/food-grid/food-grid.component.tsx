38b7e7c34de4462ef71bdc4e54e21d5971d7611d
Asra <imcengineer03@gmail.com>
2024-10-12 19:17:00 +0330

    feat: add toShow props to the grids

--- src/components/(products)/food-grid/food-grid.component.tsx
+++ src/components/(products)/food-grid/food-grid.component.tsx
@@ -1,6 +1,10 @@
-export default function FoodGrid() {
+interface FoodGridProps {
+  foods: React.ReactNode
+}
+export default function FoodGrid(props : FoodGridProps) {
   return (
     <div className='mt-4 mx-5 grid grid-cols-2 gap-7 items-center text-center'>
+      {props.foods}
     </div>
   );
 }
