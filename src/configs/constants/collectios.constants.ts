4163932ebdbb280b5a60447a8395bf420f859074
Asra <imcengineer03@gmail.com>
2024-10-12 14:08:06 +0330

    chore: remove "best" category

--- src/configs/constants/collectios.constants.ts
+++ src/configs/constants/collectios.constants.ts
@@ -2,12 +2,10 @@ export enum AllCollections {
   //Discounted = 'discounted',
   MostPopular = 'most_popular',
   MostSelling = 'most_selling',
-  Best = 'best',
 }
 
 export const allCollections = [
   //AllCollections.Discounted,
   AllCollections.MostPopular,
   AllCollections.MostSelling,
-  AllCollections.Best,
 ];
