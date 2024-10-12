870d4be704d789a4d26f1be2d1ce689dcc023bee
Asra <imcengineer03@gmail.com>
2024-10-12 19:17:44 +0330

    feat: add requests

--- src/app/(private)/(home)/(see-all)/[collection]/page.tsx
+++ src/app/(private)/(home)/(see-all)/[collection]/page.tsx
@@ -1,22 +1,65 @@
 'use client';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 
 import FoodGrid from '@/components/(products)/food-grid/food-grid.component';
 import RestaurantGrid from '@/components/(products)/restaurant-grid/restaurant-grid.component';
+import Food from '@/components/food/food.component';
 import FoodCategoryGroup from '@/components/food-category-group/food-category-group.component';
 import Heading from '@/components/heading/heading.component';
+import Restaurant from '@/components/restaurant/restaurant.component';
 import { useUnsafeI18n } from '@/locales/check-key';
 import { useI18n } from '@/locales/client';
+import { FoodService } from '@/services/food/food.service';
 import { cn } from '@/utils/cn';
 
-export default function SeeAll() {
+interface SeeAllProps {
+  params: {
+    collection: string;
+  };
+}
+export default function SeeAll(props: SeeAllProps) {
   const t = useI18n();
   const ut = useUnsafeI18n();
   const pathname = usePathname();
   const pageHeading = pathname.split('/').pop()?.toString();
+  const collection = props.params.collection;
 
   const [toShow, setToShow] = useState('food');
+
+  const { data: collectionFoods } = useSuspenseQuery({
+    queryKey: ['/food', collection],
+    queryFn: () =>
+      FoodService.getFoodSearch({
+        collection: collection,
+        page: 2,
+        per_page: 5,
+      }),
+  });
+
+  const { data: collectionRestaurants } = useSuspenseQuery({
+    queryKey: ['/restaurant', collection],
+    queryFn: () =>
+      FoodService.getFoodSearch({
+        collection: collection,
+        page: 2,
+        per_page: 5,
+      }),
+  });
+
+  const foods = collectionFoods.result.map((item) => (
+    <Food props={item} key={item.id} />
+  ));
+
+  const restaurants = collectionRestaurants.result.map((item) => (
+    <Restaurant
+      id={item.id}
+      name={item.name}
+      image={item.image}
+      key={item.id}
+    />
+  ));
 
   return (
     <>
@@ -48,7 +91,11 @@ export default function SeeAll() {
         </button>
       </div>
 
-      {toShow === 'food' ? <FoodGrid /> : <RestaurantGrid />}
+      {toShow === 'food' ? (
+        <FoodGrid foods={foods} />
+      ) : (
+        <RestaurantGrid restaurants={restaurants} />
+      )}
     </>
   );
 }
